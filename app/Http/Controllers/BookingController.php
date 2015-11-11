2b91cd4b8c3189863f7ecc02bfc8fc8138464d40
Joost Melkert <joost_melkert@hotmail.com>
2015-11-11 14:23:41 +0100

    Added main controller

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
+class BookingController extends Controller
+{
+    public function index() {
+        return view('booking.index');
+    }
+}
---
+++ resources/views/booking/index.blade.php
@@ -0,0 +1,4 @@
+@extends('index')
+@section('main')
+    Test123
+@stop
