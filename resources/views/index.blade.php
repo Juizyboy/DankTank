d77a7a76731fe5c8671e9288cfb89251ba7d74ee
Thijs vd Eerden <thijsvdeerden@gmail.com>
2015-11-11 14:53:00 +0100

    committed bruh.2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,36 +1,125 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-    @include('baseContent.head')
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Fixed fullscreen backgrounds - fullPage.js</title>
+    <meta name="author" content="Alvaro Trigo Lopez" />
+    <meta name="description" content="fullPage fixed full-screen backgrounds." />
+    <meta name="keywords"  content="fullpage,jquery,demo,screen,fixed,fullscreen,backgrounds,full-screen" />
+    <meta name="Resource-type" content="Document" />
+    <link href="{{url("plugins/fullpage/jquery.fullpage.min.css")}}" rel="stylesheet" type="text/css" />
+    <link href="{{url("plugins/fullpage/examples.css")}}" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="{{url("plugins/fullpage/jquery.fullpage.min.js")}}"></script>
+    <script type="text/javascript" src="{{url("plugins/fullpage/examples.js")}}"></script>
+    <style>
+
+        /* Style for our header texts
+        * --------------------------------------- */
+        h1{
+            font-size: 5em;
+            font-family: arial,helvetica;
+            color: #fff;
+            margin:0;
+            padding:0;
+        }
+
+        /* Centered texts in each section
+        * --------------------------------------- */
+        .section{
+            text-align:center;
+        }
+
+
+        /* Backgrounds will cover all the section
+        * --------------------------------------- */
+        #section0,
+        #section1,
+        #section2,
+        #section3{
+            background-size: cover;
+            background-attachment: fixed;
+        }
+
+        /* Defining each sectino background and styles
+        * --------------------------------------- */
+        #section0{
+            background-image: url(imgs/bg1.jpg);
+            padding: 30% 0 0 0;
+        }
+        #section2{
+            background-image: url(imgs/bg3.jpg);
+            padding: 6% 0 0 0;
+        }
+        #section3{
+            background-image: url(imgs/bg4.jpg);
+            padding: 6% 0 0 0;
+        }
+        #section3 h1{
+            color: #000;
+        }
+
+
+        /*Adding background for the slides
+       * --------------------------------------- */
+        #slide1{
+            background-image: url(imgs/bg2.jpg);
+            padding: 6% 0 0 0;
+        }
+        #slide2{
+            background-image: url(imgs/bg5.jpg);
+            padding: 6% 0 0 0;
+        }
+
+
+        /* Bottom menu
+        * --------------------------------------- */
+        #infoMenu li a {
+            color: #fff;
+        }
+    </style>
+
+    <!--[if IE]>
+    <script type="text/javascript">
+        var console = { log: function() {} };
+    </script>
+    <![endif]-->
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+
+    <script type="text/javascript" src="../jquery.fullPage.js"></script>
+    <script type="text/javascript" src="examples.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#fullpage').fullpage({
+                verticalCentered: false,
+
+                //to avoid problems with css3 transforms and fixed elements in Chrome, as detailed here: https://github.com/alvarotrigo/fullPage.js/issues/208
+                css3:false
+            });
+        });
+    </script>
+
 </head>
-<body class="skin-blue sidebar-mini layout-boxed">
-<div class="wrapper">
+<body>
 
-    <header class="main-header">
-        <!-- Logo -->
-        <a href="{{url('/')}}" class="logo">
+<a href="https://github.com/alvarotrigo/fullPage.js" id="githubLink"><img style="position: fixed;z-index:99; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
 
-            <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>H</b></span>
-            <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Hallux</b></span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        @include('baseContent.headnav')
-    </header>
-    <!-- Left side column. contains the logo and sidebar -->
-    @include('baseContent.nav')
+<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://alvarotrigo.com/fullPage/" data-text="Great plugin to create fullscreen scrolling websites: http://alvarotrigo.com/fullPage/">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-    @yield('main')
 
-    </div><!-- /.content-wrapper -->
-    @include('baseContent.footer')
+<div id="fullpage">
+    <div class="section " id="section0"><h1>fullPage.js</h1></div>
+    <div class="section" id="section1">
+        <div class="slide" id="slide1"><h1>Slide Backgrounds</h1></div>
+        <div class="slide" id="slide2"><h1>Totally customizable</h1></div>
+    </div>
+    <div class="section" id="section2"><h1>Lovely images <br />for a lovely page</h1></div>
+    <div class="section" id="section3"><h1>One Image = One thousand words</h1></div>
+</div>
 
-</div><!-- ./wrapper -->
-
-@include('baseContent.scripts')
 </body>
 </html>
